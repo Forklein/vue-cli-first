380735b5d153f61d24b0664385d1992934527271
Forklein <giuseppepisani2@gmail.com>
2021-09-03 16:19:05 +0200

    Add components and template

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <div class="credit">
+      <h1>Made by {{ author }}</h1>
+      <p>Vue JS CLI</p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  components: "Hero",
+  data() {
+    return {
+      author: "Forklein",
+    };
+  },
+};
+</script>
+
+<style>
+.credit {
+  padding: 20px;
+  font-style: italic;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <img
+      src="https://www.netfarm.it/web/image/4973/vuejs.png"
+      alt="logo"
+      style="width: 250px"
+    />
+    <h1>La mia prima app con Vue CLI!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  components: "Title",
+};
+</script>
+
+<style>
+</style>
